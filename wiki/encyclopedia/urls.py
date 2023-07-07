c1c2400ae1505c3fb52ef7951bcaf5baea1c6b97
Qildeli <anaqildeli@gmail.com>
2023-07-07 20:28:16 +0400

    Add new functionalities: Search, New Page, Edit Page

---
+++ wiki/encyclopedia/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("search", views.search, name="search"),
+    path("new", views.new, name="new"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("edit/<str:title>", views.edit, name="edit")
+]
